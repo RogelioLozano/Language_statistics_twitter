16e27804d2e6ffa39a4dfc5604bbf710cf42170e
Rogelio Lozano <rogr2012.l@gmail.com>
2020-01-31 12:10:44 -0600

    Modificado completo y por fin arreglado bug en radnom_selection_tweets!

--- random_selection_tweets.py
+++ random_selection_tweets.py
@@ -1,9 +1,6 @@
 import os 
-import gzip
-import json
 import random
 import numpy as np
-from itertools import repeat
 import pandas as pd
 import csv
 
@@ -17,14 +14,6 @@ max_tweets_UK = 99903 # respecto a camden
 countries = ["Mexico", "United_Kingdom"]
 Tweets_country = {"Mexico":max_tweets_Mex,"United_Kingdom":max_tweets_UK}
 levels = [-1,0]
-
-# Implementar una funcion que solo se ejecute una vez dentro del loop enumerate files
-def onlyonce(firstTime = [],data,persample,residue):
-    if (firstTime == []) and (data.shape[0] > (persample+residue)):
-        firstTime.append('Not Empty')
-        return True
-    else:
-        return False
 
 for country in countries:
     for admin_level in levels:
@@ -40,20 +29,10 @@ for country in countries:
         alltweets = pd.concat(all_csvs,ignore_index=True)
         alltweets = alltweets.sample(n=Tweets_country[country])
 
+        out_path = os.path.join(os.getenv("HOME"),'Datos_correctos','Tweets_filtadosporRegion','normalizados_region',country,'Level_{}'.format(admin_level),'3hourly_csv_files',"")
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
         
-
-        for file in files:
-            datos = pd.read_csv(os.path.join(path,file),sep='\t',quoting=csv.QUOTE_NONE)
-            if not datos.empty:
-                if onlyonce(data=datos,persample=persample,residue=residue):
-                    datos = datos.sample(n=persample+residue)
-                elif datos.shape[0]>persample:
-                    datos = datos.sample(n=persample)
-                else:
-
-
-            out_path = os.path.join(os.getenv("HOME"),'Datos_correctos','Tweets_filtadosporRegion','normalizados_region',country,'Level_{}'.format(admin_level),'3hourly_csv_files',"")
-            if not os.path.exists(out_path):
-                os.makedirs(out_path)
-            datos.to_csv(  os.path.join( out_path ,file ) ,index=False,sep="\t" )
-
+        for ti in alltweets["Time Interval"].unique():
+            towrite = datos[ datos["Time Interval"] == ti ]
+            towrite.to_csv( os.path.join(out_path,"{}.csv".format(ti)), index=False, sep="\t" )
